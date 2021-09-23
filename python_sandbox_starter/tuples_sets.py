b939117e315482b423fdfd98ff78ccfe4fbd296e
Julie Stocks <julie.stocks@gmail.com>
2021-09-23 15:21:59 +0900

    sets

--- python_sandbox_starter/tuples_sets.py
+++ python_sandbox_starter/tuples_sets.py
@@ -22,3 +22,23 @@ print(len(fruits))
 # print(fruits, type(fruits2))
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+
+# Create a set
+fruits_set = {'Apples', 'Oranges', 'Mango'}
+
+# Check if in set
+print('Apples' in fruits_set)
+
+# Add to set
+fruits_set.add('Grape')
+
+# Remove from set
+fruits_set.remove('Grape')
+
+# Clear set
+fruits_set.clear()
+
+# Delete
+del fruits_set
+
+print(fruits_set)
