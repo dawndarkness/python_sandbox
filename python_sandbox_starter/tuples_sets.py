0fa377da211b2593fe0d93930350098b392eec12
Julie Stocks <julie.stocks@gmail.com>
2021-09-23 13:27:50 +0900

    tuples

--- python_sandbox_starter/tuples_sets.py
+++ python_sandbox_starter/tuples_sets.py
@@ -1,5 +1,24 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
  
+# Create tuple (use paratheses)
+fruits = ('Apples', 'Oranges', 'Grapes')
+# fruits2 = tuple(('Apples', 'Oranges', 'Grapes'))
 
+# Single value needs trailing comma
+fruits2 = ('Apples',)
+
+# Get value
+print(fruits[1])
+
+# Can't change value
+# fruits[0] = 'Pears'
+
+# Delete tuple
+del fruits2
+
+# Get length
+print(len(fruits))
+
+# print(fruits, type(fruits2))
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
